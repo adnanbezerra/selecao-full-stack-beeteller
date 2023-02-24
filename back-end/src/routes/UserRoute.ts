14103dfeeec9baf6675841581b05b607e377c8cf
seu nome <adnanbezerra@gmail.com>
2023-02-24 19:22:59 -0300

    feat(back): create signin route

--- back-end/src/routes/UserRoute.ts
+++ back-end/src/routes/UserRoute.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { createNewUser } from '../controllers/UserController';
+import { createNewUser, signIn } from '../controllers/UserController';
 import { validateSchema } from '../middlewares/ValidateSchema';
 import { NewUserSchema } from '../schemas/NewUserSchema';
+import { SignInSchema } from '../schemas/SignInSchema';
 
 export const UserRoute = Router();
 
 UserRoute.post('/sign-up', validateSchema(NewUserSchema), createNewUser);
+UserRoute.post('/sign-in', validateSchema(SignInSchema), signIn);
