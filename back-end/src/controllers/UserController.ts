b8fc72845189e841fe488d63d2611d5a6d3a590e
seu nome <adnanbezerra@gmail.com>
2023-02-24 19:22:51 -0300

    feat(back): create signin route controller

--- back-end/src/controllers/UserController.ts
+++ back-end/src/controllers/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { userRepository } from '../repositories/UserRepositoryImpl';
 import { userServices } from '../services/UserServices';
-import { NewUserType } from '../types/UserTypes';
+import { NewUserType, SignInInfo } from '../types/UserTypes';
 
 export async function createNewUser(req: Request, res: Response) {
   const newUserInfo: NewUserType = req.body;
@@ -9,4 +9,12 @@ export async function createNewUser(req: Request, res: Response) {
   await userServices.createNewUser(newUserInfo, userRepository);
 
   res.sendStatus(201);
+}
+
+export async function signIn(req: Request, res: Response) {
+  const loginInfo: SignInInfo = req.body;
+
+  const token = await userServices.signIn(loginInfo, userRepository);
+
+  res.status(200).send(token);
 }
