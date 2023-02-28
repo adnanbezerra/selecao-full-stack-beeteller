662b38d89c20c80b6fcd460b384295bb53799994
seu nome <adnanbezerra@gmail.com>
2023-02-28 15:38:43 -0300

    fix(front): import env variables correctly w/vite

--- front-end/src/mock/data.js
+++ front-end/src/mock/data.js
@@ -6,6 +6,6 @@ function config(token) {
   }
 }
 
-const BASE_URL = process.env.BASE_URL;
+const BASE_URL = import.meta.env.REACT_APP_BASE_URL;
 
-export { config, BASE_URL }
+export { config, BASE_URL };
